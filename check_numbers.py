a3a8ba38c05741418c1a29e53a2079482b454453
Andy Lulham <a.lulham@gmail.com>
2015-05-10 23:56:18 +0100

    Add handy number checker script

---
+++ check_numbers.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import feedparser
+
+url = "https://edit.yournextmp.com/results/all.atom"
+feed = feedparser.parse(url)
+
+entries = {x['post_id']: x for x in feed.entries}.values()
+
+results = {}
+for x in entries:
+    if x['winner_party_name'] not in results:
+        results[x['winner_party_name']] = 0
+    results[x['winner_party_name']] += 1
+
+for x, y in results.items():
+    print x, y
+
+print len(entries)
