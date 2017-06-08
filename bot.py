3754b7b399e8595a66b1666561628d8200f13d53
Andy Lulham <a.lulham@gmail.com>
2017-06-08 15:53:48 +0100

    Minor updates

--- bot.py
+++ bot.py
@@ -1,109 +1,109 @@
-# -*- coding: utf-8 -*-
 import csv
+import json
 import re
 import os
-import time
-import json
 import pickle
-import urllib
 import sys
+import time
+import urllib
 
 import feedparser
-import requests
-import twitter
 import redis
+import requests
 from slugify import slugify
+
+import twitter
 
 
 # Abbreviated party names, for 140 character convenience
 PARTY_LOOKUP = {
-    "Labour Party": "#Labour",
-    "Labour and Co-operative Party": "#Labour / Co-op",
-    "Conservative Party": "#Conservative",
-    "Liberal Democrats": "#LibDems",
-    "Scottish Green Party": "#Greens",
-    "Green Party": "#Greens",
-    "UK Independence Party (UK I P)": "#UKIP",
-    "UK Independence Party (UKIP)": "#UKIP",
-    "Democratic Unionist Party - D.U.P.": "#DUP",
-    "Scottish National Party (SNP)": "#SNP",
-    "Plaid Cymru - The Party of Wales": "#Plaid15",
-    "SDLP (Social Democratic & Labour Party)": "#SDLP",
-    "The Respect Party": "#RespectParty",
+    'Labour Party': '#Labour',
+    'Labour and Co-operative Party': '#Labour / Co-op',
+    'Conservative Party': '#Conservative',
+    'Liberal Democrats': '#LibDems',
+    'Scottish Green Party': '#Greens',
+    'Green Party': '#Greens',
+    'UK Independence Party (UK I P)': '#UKIP',
+    'UK Independence Party (UKIP)': '#UKIP',
+    'Democratic Unionist Party - D.U.P.': '#DUP',
+    'Scottish National Party (SNP)': '#SNP',
+    'Plaid Cymru - The Party of Wales': '#Plaid15',
+    'SDLP (Social Democratic & Labour Party)': '#SDLP',
+    'The Respect Party': '#RespectParty',
 
-    "Traditional Unionist Voice - TUV": "TUV",
-    "The Eccentric Party of Great Britain": "Eccentric Party",
-    "British National Party": "BNP",
-    "The Socialist Party of Great Britain": "Socialist Party",
-    "Alliance - Alliance Party of Northern Ireland": "Alliance",
-    "Pirate Party UK": "Pirate Party",
-    "Alter Change - Politics. Only Different": "Alter Change",
-    "Christian Party \"Proclaiming Christ's Lordship\"": "Christian",
-    "Lincolnshire Independents Lincolnshire First": "Lincolnshire Ind.",
-    "An Independence from Europe": "Independence from Europe",
-    "People First - Gwerin Gyntaf": "People First",
-    "Mebyon Kernow - The Party for Cornwall": "Mebyon Kernow",
-    "Communist Party of Britain": "Communist",
-    "Speaker seeking re-election": "Speaker",
-    "Liberty Great Britain": "Liberty GB",
-    "National Liberal Party - True Liberalism": "National Liberal",
-    "Official Monster Raving Loony Party": "Raving Loony",
-    "Trade Unionist and Socialist Coalition": "TUSC",
-    "Young People's Party YPP": "Young People's Party",
-    "Communist League Election Campaign": "Communist League",
-    "The Peace Party - Non-violence, Justice, Environment": "Peace Party",
-    "Left Unity - Trade Unionists and Socialists": "Left Unity / TUSC",
-    "Cannabis is Safer than Alcohol": "CISTA",
-    "Magna Carta Conservation Party Great Britain": "Magna Carta Conservation",
-    "Christian Peoples Alliance": "CPA",
-    "Restore the Family For Children's Sake": "Restore the Family",
-    "Red Flag - Anti-Corruption": "Red Flag",
-    "Al-Zebabist Nation of Ooog": "Ooog",
-    "Children of the Atom": "Atom",
-    "Bournemouth Independent Alliance": "BIA",
+    'Traditional Unionist Voice - TUV': 'TUV',
+    'The Eccentric Party of Great Britain': 'Eccentric Party',
+    'British National Party': 'BNP',
+    'The Socialist Party of Great Britain': 'Socialist Party',
+    'Alliance - Alliance Party of Northern Ireland': 'Alliance',
+    'Pirate Party UK': 'Pirate Party',
+    'Alter Change - Politics. Only Different': 'Alter Change',
+    'Christian Party "Proclaiming Christ\'s Lordship': 'Christian',
+    'Lincolnshire Independents Lincolnshire First': 'Lincolnshire Ind.',
+    'An Independence from Europe': 'Independence from Europe',
+    'People First - Gwerin Gyntaf': 'People First',
+    'Mebyon Kernow - The Party for Cornwall': 'Mebyon Kernow',
+    'Communist Party of Britain': 'Communist',
+    'Speaker seeking re-election': 'Speaker',
+    'Liberty Great Britain': 'Liberty GB',
+    'National Liberal Party - True Liberalism': 'National Liberal',
+    'Official Monster Raving Loony Party': 'Raving Loony',
+    'Trade Unionist and Socialist Coalition': 'TUSC',
+    'Young People\'s Party YPP': 'Young People\'s Party',
+    'Communist League Election Campaign': 'Communist League',
+    'The Peace Party - Non-violence, Justice, Environment': 'Peace Party',
+    'Left Unity - Trade Unionists and Socialists': 'Left Unity / TUSC',
+    'Cannabis is Safer than Alcohol': 'CISTA',
+    'Magna Carta Conservation Party Great Britain': 'Magna Carta Conservation',
+    'Christian Peoples Alliance': 'CPA',
+    'Restore the Family For Children\'s Sake': 'Restore the Family',
+    'Red Flag - Anti-Corruption': 'Red Flag',
+    'Al-Zebabist Nation of Ooog': 'Ooog',
+    'Children of the Atom': 'Atom',
+    'Bournemouth Independent Alliance': 'BIA',
 }
 
 CONSTITUENCY_LOOKUP = {
-    "South Basildon and East Thurrock": "South Basildon",
-    "Carmarthen West and South Pembrokeshire": "Carmarthen West",
-    "Caithness, Sutherland and Easter Ross": "Caithness",
-    "Cumbernauld, Kilsyth and Kirkintilloch East": "Cumbernauld",
-    "East Kilbride, Strathaven and Lesmahagow": "East Kilbride",
-    "Dumfriesshire, Clydesdale and Tweeddale": "Dumfriesshire",
-    "Inverness, Nairn, Badenoch and Strathspey": "Inverness",
-    "Middlesbrough South and East Cleveland": "Middlesbrough South",
-    "Dumfriesshire, Clydesdale and Tweeddale": "Dumfriesshire",
-    "Normanton, Pontefract and Castleford": "Normanton",
+    'South Basildon and East Thurrock': 'South Basildon',
+    'Carmarthen West and South Pembrokeshire': 'Carmarthen West',
+    'Caithness, Sutherland and Easter Ross': 'Caithness',
+    'Cumbernauld, Kilsyth and Kirkintilloch East': 'Cumbernauld',
+    'East Kilbride, Strathaven and Lesmahagow': 'East Kilbride',
+    'Dumfriesshire, Clydesdale and Tweeddale': 'Dumfriesshire',
+    'Inverness, Nairn, Badenoch and Strathspey': 'Inverness',
+    'Middlesbrough South and East Cleveland': 'Middlesbrough South',
+    'Dumfriesshire, Clydesdale and Tweeddale': 'Dumfriesshire',
+    'Normanton, Pontefract and Castleford': 'Normanton',
 }
 
 # Abbreviate party names, using lookup and other hacks
 def abbrev_party(party):
     if party in PARTY_LOOKUP:
         return PARTY_LOOKUP[party]
-    if party.endswith(" Party"):
+    if party.endswith(' Party'):
         party = party[:-6]
-    if party.startswith("The "):
+    if party.startswith('The '):
         party = party[4:]
     return party
 
 def abbrev_constituency(constituency):
     constituency = CONSTITUENCY_LOOKUP.get(constituency, constituency)
-    return constituency.replace(u' and ', u' & ')
+    return constituency.replace(' and ', ' & ')
 
 # fetch locations (for geopositioning tweets)
-with open("locations.json") as f:
+with open('locations.json') as f:
     locations = json.load(f)
 
-# print "fetching redis ..."
+# print('fetching redis ...')
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 r = redis.from_url(redis_url)
 
 def parse_feed():
-    # print "fetching results feed ..."
-    feed = feedparser.parse(os.getenv('FEED_URL'))
+    # print('fetching results feed ...')
+    feed = feedparser.parse('https://candidates.democracyclub.org.uk/results/all.atom')
 
-    api_tmpl = "http://yournextmp.popit.mysociety.org/api/v0.1/persons/{}"
-    status_tmpl = u"{constituency}! #YourNextMP is: {person_name} ({party}) https://yournextmp.com/person/{person_id}/{slug}{twitter_str}"
+    api_tmpl = 'http://yournextmp.popit.mysociety.org/api/v0.1/persons/{}'
+    status_tmpl = '{constituency}! #YourNextMP is: {person_name} ({party}) https://yournextmp.com/person/{person_id}/{slug}{twitter_str}'
 
     winners = {item['post_id']: item['winner_popit_person_id'] for item in feed.entries}
 
@@ -119,10 +119,10 @@ def parse_feed():
                 # we've already tweeted the winner here
                 continue
             else:
-                print "delete old tweet: {}".format(tweeted['tweet_id'])
+                print('delete old tweet: {}'.format(tweeted['tweet_id']))
                 t.delete(tweeted['tweet_id'])
                 if tweeted['twitter_handle']:
-                    print "remove old twitter handle: @{}".format(tweeted['twitter_handle'])
+                    print('remove old twitter handle: @{}'.format(tweeted['twitter_handle']))
                     _ = t.remove_from_list(os.getenv('TWITTER_LIST_ID'), tweeted['twitter_handle'])
         if id_ != winners[item['post_id']]:
             # don't tweet this - it's not the winner!
@@ -131,13 +131,13 @@ def parse_feed():
         person = requests.get(api_tmpl.format(id_), verify=False).json()['result']
 
         if person.get('proxy_image'):
-            kw['filename'] = "{}.png".format(id_)
+            kw['filename'] = '{}.png'.format(id_)
             # fetch the image
-            urllib.urlretrieve("{}/200/0".format(person['proxy_image']), kw['filename'])
+            urllib.urlretrieve('{}/200/0'.format(person['proxy_image']), kw['filename'])
 
         # cc the candidate (if they're on twitter)
         twitter_handle = person['versions'][0]['data'].get('twitter_username')
-        twitter_str = " @{}".format(twitter_handle) if twitter_handle else ""
+        twitter_str = ' @{}'.format(twitter_handle) if twitter_handle else ''
         if twitter_handle:
             _ = t.add_to_list(os.getenv('TWITTER_LIST_ID'), twitter_handle)
 
@@ -153,15 +153,16 @@ def parse_feed():
             kw['lat'], kw['long'] = l
 
         # Send a tweet here!
-        print "Tweeting:", kw
+        print('Tweeting:')
+        print(json.dumps(kw, indent=4))
         tweet = t.tweet(**kw)
 
         if tweet:
             # Save the tweet to redis
             r.set(item['post_id'], pickle.dumps({
-                "person_id": item['winner_popit_person_id'],
-                "tweet_id": tweet.id,
-                "twitter_handle": twitter_handle,
+                'person_id': item['winner_popit_person_id'],
+                'tweet_id': tweet.id,
+                'twitter_handle': twitter_handle,
             }))
 
         if kw.get('filename'):
@@ -171,7 +172,7 @@ def parse_feed():
         time.sleep(1)
 
 while True:
-    print "Working ..."
+    print('Working ...')
     parse_feed()
-    print "Sleeping ..."
+    print('Sleeping ...')
     time.sleep(60)
