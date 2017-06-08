5a1482437842430ad0b8cdd12c5a8b1832ac218a
Andy Lulham <a.lulham@gmail.com>
2017-06-08 15:36:07 +0100

    Update gen script & json of WMC centroids

--- gen_locations.py
+++ gen_locations.py
@@ -1,14 +1,20 @@
+import json
 import time
+
 import requests
 
 
-mapit_ids = requests.get("http://mapit.mysociety.org/areas/WMC").json().keys()
+mappings = requests.get('http://firefly.ukcod.org.uk/~mark/ynr-post-mapping.json').json()
+post_id_lookup = {m['old']: m['new'] for m in mappings}
+
+mapit_ids = requests.get('https://mapit.mysociety.org/areas/WMC').json().keys()
 locations = {}
 for mapit_id in mapit_ids:
     time.sleep(0.5)
-    j = requests.get("http://mapit.mysociety.org/area/%s/geometry" % mapit_id).json()
-    if "centre_lat" in j:
-        locations[mapit_id] = (j["centre_lat"], j["centre_lon"])
+    j = requests.get('http://mapit.mysociety.org/area/%s/geometry' % mapit_id).json()
+    if 'centre_lat' in j:
+        post_id = post_id_lookup[mapit_id]
+        locations[post_id] = (j['centre_lat'], j['centre_lon'])
 
-with open("locations.json", "w") as f:
+with open('locations.json', 'w') as f:
     json.dump(locations, f)
